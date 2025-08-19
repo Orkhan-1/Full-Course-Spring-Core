1671c39cc6041b24fdcce688057dbc90d64773d7
Orkhan <gasanov.orkhan1@gmail.com>
2025-08-18 23:13:43 -0700

    [Tutorial 6] Spring Configuration - Annotation-based Configuration

--- SpringApp.java
+++ SpringApp.java
@@ -1,16 +1,14 @@
-import com.orkhangasanov.CustomerRepository;
 import com.orkhangasanov.CustomerService;
-import com.orkhangasanov.SpringAppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringApp {
     public static void main(String[] args) {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(SpringAppConfig.class);
-        CustomerService service = context.getBean(CustomerService.class);
-        service.registerCustomer("Java Config Customer");
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.orkhangasanov");
 
+        CustomerService service = context.getBean(CustomerService.class);
+        service.registerCustomer("Annotation Config Customer");
     }
 }
