4394ab76ae4a536982c02eb75ffbc0f5a04a7bbd
Orkhan <gasanov.orkhan1@gmail.com>
2025-08-19 20:10:57 -0700

    [Tutorial 7] Autowired annotation - @Primary annotation

--- com/orkhangasanov/autowired/SMSNotification.java
+++ com/orkhangasanov/autowired/SMSNotification.java
@@ -1,8 +1,10 @@
 package com.orkhangasanov.autowired;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class SMSNotification implements NotificationService {
     public void send(String message) {
         System.out.println("Sending SMS: " + message);
--- com/orkhangasanov/autowired/UserService.java
+++ com/orkhangasanov/autowired/UserService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     @Autowired
-    @Qualifier("emailNotification")
     private NotificationService notificationService;
 
     public void register(String name) {
