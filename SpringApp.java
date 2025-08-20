d034c405c49f4b875649e98dcb71d0335792be84
Orkhan <gasanov.orkhan1@gmail.com>
2025-08-19 20:33:06 -0700

    [Tutorial 9] Externalizing Configuration

--- SpringApp.java
+++ SpringApp.java
@@ -1,13 +1,13 @@
-import com.orkhangasanov.beanlifecycle.DemoBean;
-import org.springframework.context.ConfigurableApplicationContext;
+import com.orkhangasanov.configdemo.AppConfig;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SpringApp {
     public static void main(String[] args) {
 
-        ConfigurableApplicationContext context =
-                new AnnotationConfigApplicationContext("com.orkhangasanov");
-        DemoBean bean = context.getBean(DemoBean.class);
-        context.close();
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.orkhangasanov.configdemo");
+        AppConfig config = context.getBean(AppConfig.class);
+        System.out.println(config.getName() + " v " + config.getVersion());
     }
 }
---
+++ com/orkhangasanov/configdemo/AppConfig.java
@@ -0,0 +1,42 @@
+package com.orkhangasanov.configdemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:application.properties")
+public class AppConfig {
+
+    @Value("${app.name}")
+    private String name;
+    @Value("${app.version}")
+    private String version;
+    @Value("${app.support.email}")
+    private String supportEmail;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getSupportEmail() {
+        return supportEmail;
+    }
+
+    public void setSupportEmail(String supportEmail) {
+        this.supportEmail = supportEmail;
+    }
+
+}
